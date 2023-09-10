d559b9e40465e969fc31bf778f608cca8d49e2d4
Shiva <kshiva.sdet@gmail.com>
2023-09-10 12:44:55 -0500

    New Class Added to validate TestNG priority

---
+++ Tools/src/test/java/Configuration/Tools/PriorityTest.java
@@ -0,0 +1,18 @@
+package Configuration.Tools;
+
+import org.testng.annotations.Test;
+
+public class PriorityTest {
+	
+	@Test(priority=2)
+	public void test1()
+	{
+		System.out.println("Test2");
+	}
+	
+	@Test(priority=1)
+	public void test2()
+	{
+		System.out.println("Test1");
+	}
+}
